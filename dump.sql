ccd5a901a5b9f693abd5b3a8648988b87d4faa80
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-12-20 17:25:10 -0300

    fix dump

--- dump.sql
+++ dump.sql
@@ -2,12 +2,12 @@ CREATE TABLE "questions" (
 	"id" serial NOT NULL,
 	"question" TEXT NOT NULL,
 	"student" varchar(255) NOT NULL,
-	"class" varchar(3) NOT NULL,
+	"classs" varchar(3) NOT NULL,
 	"tags" varchar(255) NOT NULL,
 	"submitedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
 	"answer" TEXT,
 	"answeredBy" int,
-	"asweredAt" TIMESTAMP WITH TIME ZONE,
+	"answeredAt" TIMESTAMP WITH TIME ZONE,
 	CONSTRAINT "questions_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -23,8 +23,7 @@ CREATE TABLE "users" (
   OIDS=FALSE
 );
 
-ALTER TABLE "questions" ADD CONSTRAINT "questions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-ALTER TABLE "questions" ADD CONSTRAINT "questions_fk1" FOREIGN KEY ("answered_by") REFERENCES "users"("id");
+ALTER TABLE "questions" ADD CONSTRAINT "questions_fk1" FOREIGN KEY ("answeredBy") REFERENCES "users"("id");
 
 
 
